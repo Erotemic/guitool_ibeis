dbc737f68bd502485e775e402c2d828956b6fe7f
joncrall <crallj@rpi.edu>
2014-05-25 18:11:02 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
 
+INSTALL_REQUIRES = [
+    #'PyQt4'  # non pipi index
+]
+
 if __name__ == '__main__':
     from utool.util_setup import setuptools_setup
     setuptools_setup(
         name='guitool',
-        version='1.0.0.dev1',
         description=('Guitool - tools pyqt4 guis'),
         url='https://github.com/Erotemic/guitool',
         keywords='',
         package_data={},
         classifiers=[],
         author='Jon Crall',
+        install_requires=INSTALL_REQUIRES,
         author_email='erotemic@gmail.com',
         setup_fpath=__file__,
     )
